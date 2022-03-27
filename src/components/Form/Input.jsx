6ff83aa1320c16a8c03fbc0e882a792024af1a51
unknown <joaomgoncalvesbr@gmail.com>
2022-03-27 12:11:01 -0300

    header component styled

---
+++ src/components/Form/Input.jsx
@@ -0,0 +1,22 @@
+import { FormControl, FormLabel, Input as ChakraInput } from "@chakra-ui/react";
+
+export function Input({ name, type, label, ...rest }) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        type={type}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
